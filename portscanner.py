959260e78658cf2527b841729453c955523012b2
Chad O'Donnell <odonn147@d.umn.edu>
2021-01-29 10:23:01 -0600

    Added a TCP OS Chart in portscanner.py

--- portscanner.py
+++ portscanner.py
@@ -5,6 +5,7 @@ from datetime import datetime
 
 # Credit for this file goes towards: Python for Beginners,
 # I couldn't have done it without them helping me out.
+# This helps us infer which OS the user has, comparing ports to listed chart below.
 
 # Clear the screen
 subprocess.call('clear', shell=True)
@@ -21,13 +22,18 @@ print ("-" * 60)
 # Check what time the scan started, used for finding scan time-stamps
 t1 = datetime.now()
 
-# Using the range function to specify ports (here it will scans all ports between 1 and 1024)
-# I could have made this go up to 65535, but alas my computer is very slow, don't mind me.
+# Using the range function to specify ports (here it will scans all ports between 1 and 65535)
+# TCP WINDOW SIZE CHART ->
+# LINUX:            5840
+# GOOGLE'S lINUX:   5720
+# FreeBSD:          65535
+# WINDOWS XP/10:    65535
+# WINDWOS 7/2008:   8192
+# Depending on open ports, you can infer OS.
 
 # We also put in some error handling for catching errors
-
 try:
-    for port in range(1,1025):  
+    for port in range(1,65535):  
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         result = sock.connect_ex((remoteServerIP, port))
         # If port isn't used, share that port is open
